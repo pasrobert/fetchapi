fe774e7eb99fc6c32d43004b4c9f44d8ec57232c
Robert Pasieczny <pasrobert@icloud.com>
2020-03-31 12:33:02 +0200

    app-structure

---
+++ src/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {TextField} from '@material-ui/core'
+
+const Chat = () => {
+
+    return(
+        <div>
+            Chat
+        </div>
+    )
+}
+
+export default Chat
